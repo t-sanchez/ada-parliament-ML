823089e546fdffeda5f9d297419218163928a823
barry <martin.barry@epfl.ch>
2017-01-29 10:33:32 +0100

    problem encore vieille version

--- 05-viz/chapter_1/js/votation_subj.js
+++ 05-viz/chapter_1/js/votation_subj.js
@@ -1,6 +1,7 @@
 var topics;
 var pos =[];
 var map = {};
+var passage ={};
 d3.csv("../viz_data/map_bill_ID.csv", function(mapping){
 
     mapping.forEach(function(element) {
@@ -8,17 +9,16 @@ d3.csv("../viz_data/map_bill_ID.csv", function(mapping){
 
     });
 });
-
 d3.json("../viz_data/topics.json", function(list) {
     console.log(list)
     topics = list;
     dx = 1380/topics.length;
     dy = 800/topics.length;
     for (var i = 0; i < (topics.length); i++) {
-        x = (i+1/2)%((topics.length)/2)*dx/12
-        y = 50+10*(i>=(topics.length)/2)
+        x = (i)%((topics.length)/2)*dx/12
+        y = 2//+10*(i>=(topics.length)/2)
         console.log(y)
-    pos.push([x,y])
+        pos.push([x,y])
 
     }
 });
@@ -35,9 +35,9 @@ var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
 
-    .force("charge", d3.forceManyBody().strength(-4))
-    .force("collision", d3.forceCollide().radius(radius+padding).iterations(5).strength(0.5))
-    .force("center", d3.forceCenter(width / 2, height / 2));
+//.force("charge", d3.forceManyBody().strength(-4))
+//.force("collision", d3.forceCollide().radius(radius+padding).iterations(5).strength(0.5))
+//.force("center", d3.forceCenter(width / 2, height / 2));
 
 var input = document.getElementById("countries");
 var awesomplete = new Awesomplete(input, {
@@ -68,6 +68,16 @@ d3.json("../viz_data/datasubject.json", function(error, graph) {
         .attr("fill", function(d) {
 
             return color(d.group); })
+        .attr("x", function(d) {
+            place = position(d.group)
+            if (d.group in passage)
+                passage[d.group]+=1
+            else
+                passage[d.group]=1
+            d.x = 20+(place[0])*21+(passage[d.group]%6)*17
+            d.y = (place[1])*6+Math.floor(passage[d.group]/6)*5
+            return (40-place[0])*20;})
+
         .call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
@@ -75,8 +85,8 @@ d3.json("../viz_data/datasubject.json", function(error, graph) {
         .on("mouseover",function(d) {
             d3.selectAll(".nodes").style("r", radius);
             d3.select(this).style("r", 2 * radius)
-            document.getElementById("law").innerHTML = d.id ;
-            document.getElementById("topic").innerHTML = d.group;
+            document.getElementById("councilorName").innerHTML = d.id ;
+            document.getElementById("councilorParty").innerHTML = d.group;
 
         })
         .on("mouseout",dephasis)
@@ -90,7 +100,7 @@ d3.json("../viz_data/datasubject.json", function(error, graph) {
         })
         .on("dblclick", function(d) {
             localStorage['parl'] = d.id;
-            window.location.assign("../html/viz-person.html", '_blank');
+            window.location.assign("../html/viz_person.html", '_blank');
             //window.location.assign("../html/viz-person.html", '_blank');
         });
 
@@ -105,13 +115,15 @@ d3.json("../viz_data/datasubject.json", function(error, graph) {
         .attr("class", "legend")
         .style("font-size","12px")
         .attr("transform", function(d, i) {
-            return "translate("+ (-750+i * 30) +"," + -220 + ") rotate(40) "; })
+            return "translate("+ (-900+i * 30) +"," + -450 + ") rotate(40) "; })
 
 
     legend.append("rect")
         .attr("x", width - 18)
         .attr("width", 18)
         .attr("height", 18)
+        .attr("rx", "3px")
+        .attr("ry", "3px")
         .style("fill", color);
 
     legend.append("text")
@@ -129,7 +141,7 @@ d3.json("../viz_data/datasubject.json", function(error, graph) {
         .on("tick", ticked);
 
     //simulation.force("link")
-      //  .links(graph.links);
+    //  .links(graph.links);
 
 
 
@@ -143,7 +155,7 @@ d3.json("../viz_data/datasubject.json", function(error, graph) {
         //     .attr("y2", function(d) { return d.target.y; });
 
         node
-            .each(gravity())
+        //.each(gravity())
             .attr("cx", function(d) { return d.x = Math.max(r, Math.min(width - r, d.x)); })
             .attr("cy", function(d) { return d.y= Math.max(r, Math.min(height - r, d.y)); });
 
@@ -173,14 +185,14 @@ function emphasis(d) {
     d3.select(d).style("r", 2 * radius)
         .style("visibility", "visible")
 
-    document.getElementById("law").innerHTML = d.id ;
-    document.getElementById("topic").innerHTML = d.group;
+    document.getElementById("councilorName").innerHTML = d.id ;
+    document.getElementById("councilorParty").innerHTML = d.group;
 }
 function dephasis(d) {
     d3.selectAll(".nodes").style("r", radius);
     d3.select(this).style("r",radius);
-    document.getElementById("law").innerHTML = "" ;
-    document.getElementById("topic").innerHTML = "";
+    document.getElementById("councilorName").innerHTML = "Law" ;
+    document.getElementById("councilorParty").innerHTML = "Topic";
 
 }
 
@@ -193,17 +205,17 @@ function handleKeyPress(e){
 }
 function findperson() {
     var input  =document.getElementById("countries");
-     d  = d3.select("[id='" + input.value + "']")
+    d  = d3.select("[id='" + input.value + "']")
         .style("r", 5 * radius)
-         .attr("transform", function(d) {
-             return "translate("+ (-d.x+120) +"," + (-d.y+50) + ")"; });
+        .attr("transform", function(d) {
+            return "translate("+ (-d.x+30) +"," + (-d.y+25) + ")"; });
 }
 function gravity() {
     return function(d) {
         var alpha;
         place = position(d.group)
         x = (40-place[0])*20,
-        y = (40-place[1])*20
+            y = (40-place[1])*20
         alpha = 0.05;
 
         var dx = -d.x+x//*(Math.abs(-d.x+x)>50);
